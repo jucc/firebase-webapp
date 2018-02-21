989316f8214c8630a44c49084b02caef11e752c9
jucc <jucorrea.mail@gmail.com>
2018-02-21 02:52:21 -0300

    moving script to separate file

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCIFMrfhGbcib6dl_qlvJ24DVsGxrKYirI",
+    authDomain: "quickstart-web-5ef5f.firebaseapp.com",
+    databaseURL: "https://quickstart-web-5ef5f.firebaseio.com",
+    projectId: "quickstart-web-5ef5f",
+    storageBucket: "quickstart-web-5ef5f.appspot.com",
+    messagingSenderId: "573053998886"
+};
+firebase.initializeApp(config);
+
+var h1 = document.getElementById('hotdogstatus');
+var dbRef = firebase.database().ref().child('text');
+dbRef.on('value', snap => hotdogstatus.innerText = snap.val());
